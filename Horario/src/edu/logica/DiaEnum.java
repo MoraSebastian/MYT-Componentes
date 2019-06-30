71cce08e343a318e8a7b468922b8316f6afd2b72
Estefanía <estefaniag08@hotmail.com>
2019-06-30 17:20:28 -0500

    Inicio de conexión de horario con agenda

--- Horario/src/edu/logica/DiaEnum.java
+++ Horario/src/edu/logica/DiaEnum.java
@@ -1,6 +1,25 @@
 package edu.logica;
 
 public enum DiaEnum {
-	
-	LUNES, MARTES, MIERCOLES, JUEVES, VIERNES
+	LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO;
+	public DiaEnum getEnumPorNumeroDia(int dia) {
+		switch (dia) {
+		case 0:
+			return LUNES;
+		case 1:
+			return MARTES;
+		case 2:
+			return MIERCOLES;
+		case 3:
+			return JUEVES;
+		case 4:
+			return VIERNES;
+		case 5:
+			return SABADO;
+		case 6:
+			return DOMINGO;
+		default:
+			return null;
+		}
+	}
 }
