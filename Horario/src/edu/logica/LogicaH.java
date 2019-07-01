5d5e9dda79979f96cee184b684b93231899e062d
MoraSebastian <30810920+MoraSebastian@users.noreply.github.com>
2019-06-30 22:17:58 -0500

    Clase LogicaH en estado anterior

--- Horario/src/edu/logica/LogicaH.java
+++ Horario/src/edu/logica/LogicaH.java
@@ -1,11 +1,9 @@
 package edu.logica;
 
-import java.util.ArrayList;
 
-import java.util.List;
 
 import javax.swing.JOptionPane;
-import edu.logica.DiaEnum;
+
 import edu.cableado.consultarHorario;
 import edu.cableado.consultarTarea;
 import edu.cableado.horarioEstudiante;
@@ -13,86 +11,32 @@ import edu.utilidades.Cargador;
 
 public class LogicaH implements horarioEstudiante {
 
-	private Horario horarioDia = null;
-
 	@Override
 	public void H() {
-		//Crud();	
+		Crud();	
 		JOptionPane.showMessageDialog(null, "Horario");				
 	}
 	
 	
 	public void añadirFranja(String nombreF, boolean dias[], String tipoF, int horaInicio, int horaFinal) throws Exception{
-		String datos[] = {"", ""+horaInicio, ""+horaFinal, tipoF, nombreF};	
-		Crud(datos, "bloque", dias);		
-	}
-	//REGISTRO DE FRANJA UNICAMENTE, SU RELACION CON LOS DIAS VA EN OTRO LADO
-	public void Crud(String[] datos, String tabla, boolean[] dias) {
-		Crud();
-		JOptionPane.showMessageDialog(null, "Horario");
-	}
+		for(int i = 0; i < dias.length;i++) {
+			if(dias[i]) {
 
-	private void inicializarDiasSemana() {
-		if (horarioDia == null) {
-			for (DiaEnum dia : DiaEnum.values()) {
-				horarioDia = new Dia();
-				horarioDia.setNombreDia(dia.name());
-				horarioDia.setNumeroDia(dia.getNumeroDia());
-				horarioDia.agruparHorario();
+			}else {
+				//-- En ese día no va la franja
 			}
-		} else {
-			// -- Ya se cre� el listado de d�as.
-		}
-	}
-
-	public void añadirFranja(String nombreF, boolean dias[], String tipoF, int horaInicio, int horaFinal)
-			throws Exception {
-		inicializarDiasSemana();
-		for (int i = 0; i < dias.length; i++) {
-			if (dias[i]) {
-				Franja franja;
-				System.out.println(horarioDia.obtenerDiaPorNumero(i).getNombreDia());
-				Dia diaSemana = (Dia) horarioDia.obtenerDiaPorNumero(i); 
-				franja = new Franja();
-				Franja.setLocalizador(diaSemana.getLocalizadorFranja());
-				franja.setTiempoInicio(horaInicio);
-				franja.setTiempoFinal(horaFinal);
-				List<Integer> listaHoras = new ArrayList<>();
-				for (int j = franja.getTiempoInicio(); j < franja.getTiempoFinal(); j++) {
-					listaHoras.add(j);
-				}
-				franja.setHoras(listaHoras);
-				franja.setNombreFranja(nombreF);
-				franja.setDias(dias);
-				franja.setTipoFranja(TipoFranjaEnum.valueOf(tipoF));
-				franja.agruparFranjaDia();
-				diaSemana.setLocalizadorFranja(franja);
-				Franja.setLocalizador(null);
-				// --
-			} else {
-				// -- En ese d�a no va la franja
-			}
-			System.out.println();
-		}
+		}	
 	}
 
 	public void Crud() {
-		Cargador cc = new Cargador("componentes", ClassLoader.getSystemClassLoader());
-		ArrayList<String> array;
+		Cargador cc = new Cargador("componentes", ClassLoader.getSystemClassLoader());		
 		try {
 			Class cls = cc.cargarUnaClaseDesdeSuDirectorio(consultarHorario.class.getName());
-			if (cls != null) {
-				consultarHorario ch = (consultarHorario) cls.newInstance();
-				//ENCONTRAR EL ID, YA QUE NO LO HACE POR DEFECTO
-				array = ch.consultas("maxid", tabla);
-				if(array.get(0)==null) {
-					datos[0] = "1";
-				}else {
-					datos[0] = array.get(0);
-				}
-				ch.RegistrarH(datos, tabla, dias);											
+			if(cls != null) {
+				consultarHorario ch = (consultarHorario) cls.newInstance();												
+				ch.consultaH();											
 			}
-		} catch (Exception e) {
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -110,9 +54,4 @@ public class LogicaH implements horarioEstudiante {
 		return null;
 	}
 	
-
-	public Horario getHorarioDia() {
-		return horarioDia;
-	}
-
 }
