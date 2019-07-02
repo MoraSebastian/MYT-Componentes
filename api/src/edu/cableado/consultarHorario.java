224452273071d803752116c7079693ef1d1c1876
Sebastián <morasabogalsebastian@gmail.com>
2019-07-01 20:00:34 -0500

    Implementacion de arreglo de objetos en gestorH

--- api/src/edu/cableado/consultarHorario.java
+++ api/src/edu/cableado/consultarHorario.java
@@ -6,5 +6,5 @@ public interface consultarHorario {
 	void consultaH();
 	void RegistrarH(String[] datos, String tabla, boolean[] dias);
 	ArrayList<String> consultas(String caso, String tabla);
-	public ArrayList<String[]> CtotalFranjas();
+	public ArrayList<Object[]> CtotalFranjas();
 }
